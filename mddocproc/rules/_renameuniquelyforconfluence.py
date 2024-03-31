0a55b7225577e715a86078091b9a15be89e72e53
OriginalDaemon <github@originaldaemon.com>
2024-03-31 02:16:28 +0000

    Remove unused import

--- mddocproc/rules/_renameuniquelyforconfluence.py
+++ mddocproc/rules/_renameuniquelyforconfluence.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-import os
-
 from ._base import document_rule
 
 from typing import TYPE_CHECKING
-
 if TYPE_CHECKING:  # pragma: no cover
     from .._processing import ProcessingContext
     from .._document import Document
